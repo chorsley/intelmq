3bc6bcdb2d410a0386515ba018682aafdeda05b3
Chris Horsley <chris.horsley@csirtfoundry.com>
2018-03-28 23:02:08 +1100

    Functional - needs more tests passing now

--- intelmq/bots/parsers/abusech/parser_zeustracker_rss.py
+++ intelmq/bots/parsers/abusech/parser_zeustracker_rss.py
@@ -55,7 +55,6 @@ class AbusechZeustrackerRSSParserBot(ParserBot):
             entry_bytes = ElementTree.tostring(entry, encoding='utf-8',
                                                method='xml')
             entry_str = entry_bytes.decode("utf-8")
-            # self.logger.info('entry_bytes {}!'.format(entry_str))
             yield entry_str
 
     def parse_line(self, entry_str, report):
@@ -77,30 +76,28 @@ class AbusechZeustrackerRSSParserBot(ParserBot):
             event.add('time.source',
                       datetime.replace(tzinfo=pytz.utc).isoformat())
 
-        desc = document.find('title').text
+        desc = document.find('description').text
 
         # Host: 5.101.176.115, IP address: 5.101.176.115, SBL: Not listed, status: unknown, level: 4, Malware: Citadel, AS: 198068, country: EE
-        m = re.match(r'Host: (?P<fqdn>.*), IP address: (?P<ip>.*)?, '
-                     'SBL: (?P<sbl>.*)?, status: (?P<status>.*),'
-                     'level: (?P<level>.*)?, Malware: (?P<malware>.*)?, '
-                     'AS: (?P<asn>.*)?, country:(<?P<cc>.*)?', desc)
+        m = re.match(r"""Host:\s(?P<fqdn>.*?),\sIP\saddress:\s(?P<ip>.*?)?,\s
+                     SBL:\s(?P<sbl>.*?)?,\sstatus:\s(?P<status>.*?),\s
+                     level:\s(?P<level>.*?)?,\sMalware:\s(?P<malware>.*?)?,\s
+                     AS:\s(?P<asn>.*?)?,\scountry:(?P<cc>.*?)?""", desc, re.X)
 
         if m:
-            self.logger.info("MATCH!")
-            if is_ip_address(m.group('fqdn')):
-                event.add('source.ip', m.group('fqdn'))
-            else:
+            # we get IPs as hostnames, IntelMQ won't let us add as fqdn
+            if not is_ip_address(m.group('fqdn')):
                 event.add('source.fqdn', m.group('fqdn'))
 
             if m.group('ip'):
-                event.add('source.ip', m.group('fqdn'))
+                event.add('source.ip', m.group('ip'))
             if m.group('sbl'):
                 extra['sbl'] = m.group('sbl')
             if m.group('status'):
                 event.add('status', m.group('status'))
             if m.group('level'):
                 extra['level'] = m.group('level')
-            if m.group('as'):
+            if m.group('asn'):
                 event.add('source.asn', m.group('asn'))
             if m.group('cc'):
                 event.add('source.geolocation.cc', m.group('cc'))
@@ -110,7 +107,7 @@ class AbusechZeustrackerRSSParserBot(ParserBot):
 
         event.add('classification.type', 'c&c')
         event.add("raw", entry_str)
-        self.logger.info(event)
+        # self.logger.info(event)
         yield event
 
 
